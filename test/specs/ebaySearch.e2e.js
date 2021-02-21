ffc2e5fc98d653eadd4ccb462ca125c24d9b4d4f
WandyHernandez <whernandez@localhost.localdomain>
2021-02-20 19:11:07 -0500

    Added Search to ebay page. Tested Input search and changes category

--- test/specs/ebaySearch.e2e.js
+++ test/specs/ebaySearch.e2e.js
@@ -4,5 +4,24 @@ describe('Ebay Products Search', () => {
         expect(browser).toHaveTitle(
             'Artículos electrónicos, autos, ropa, objetos de colección, cupones y más | eBay'
         );
+    });
+
+    it('Should search for a product and verify the search text value.', () => {
+        const searchInput = $('#gh-ac');
+        const searchBtn = $('#gh-btn');
+
+        searchInput.addValue('Laptop');
+        searchBtn.click();
+
+        expect(searchInput).toHaveValue('Laptop');
+    });
+
+    it('Should redirect to a new page and verify the title value.', () => {
+        expect(browser).toHaveTitle('Laptop | eBay');
+    });
+
+    it('Should update the search category', () => {
+        const categorySelect = $('#gh-cat option:nth-child(1)');
+        expect(categorySelect).toHaveText('Computadoras portátiles o laptops y netbooks');
     })
 });
